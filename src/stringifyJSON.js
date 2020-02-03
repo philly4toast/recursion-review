8265e17df15a46719ab67ebcb18aab42d6e504ff
philly4toast <phillipyoun@gmail.com>
2020-02-03 15:56:02 -0800

    Complete stringifyJSON recursion sprint

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,53 @@
 
 // but you don't so you're going to write it from scratch:
 
+
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+    
+  
+  if (typeof obj === "number") {
+    return '' + obj;
+  }
+
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (obj === true) {
+    return 'true';
+  }
+  if (obj === false) {
+    return 'false';
+  } 
+
+  if (typeof obj === 'string'){
+    return '"' + obj + '"';
+  }
+  
+  if (Array.isArray(obj)) {
+    var result = [];
+    if (obj.length === 0) {
+      return '[]'
+    }
+    for (var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]))
+    }
+  
+    return "[" + result + "]";
+
+  } else {
+    var result = [];
+    for (var key in obj) {
+      if (obj[key] === undefined) {
+        return '{}';
+      }
+      result.push('"' + key + '"' + ":" + stringifyJSON(obj[key]));
+    }
+    return "{" + result + "}";
+  }
+  
+
+}
+  // return result;
+;
+ 
