ddaccc77d635c1c042616efcbe8d3899d90c8a32
Phillip Youn <phillipyoun@gmail.com>
2020-02-03 20:29:54 -0800

    Forked and committing pair challenges

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  
+  var results = [];
+
+  var classTest = function(node) {
+  
+    if (node.classList && node.classList.contains(className)) {
+      results.push(node);
+    }
+    
+    if (node.childNodes) {
+      node.childNodes.forEach(function(elem) {
+        classTest(elem);
+
+      });
+    }
+
+  };
+
+  classTest(document.body);
+
+
+  return results;
+
 };
